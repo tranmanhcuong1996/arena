0ed3325f749dfab6d7a0c470b5c7133ab8e8096e
cuongtm <kyoudo1996@gmail.com>
2025-04-11 15:48:34 +0700

    1st commit

---
+++ character.js
@@ -0,0 +1,20 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+const REONA = {
+  name: "Reona",
+  stats: {
+    hit_point: 1000,
+  },
+};
+
+const FEIXIAO = {
+  name: "Fei Xiao",
+  stats: {
+    hit_point: 800,
+  },
+};
+
+const GAME_SETTING = {
+  turn: "user",
+};
---
+++ script.js
@@ -0,0 +1,8 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+document.querySelector("#user-champion .character-name").textContent =
+  REONA.name;
+console.log(FEIXIAO);
+document.querySelector("#opponent-champion .character-name").textContent =
+  FEIXIAO.name;
